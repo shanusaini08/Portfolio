39d402911cabb3ae2f4e07c86bc119a905f9d841
arcs-success <shanu.saini.vns@gmail.com>
2024-07-18 01:07:57 +0530

    added multiple pages and updated ui

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/', // or '/Portfolio/' if your site is under a subdirectory
+  base: '/', // Update this to your repository name
 });
